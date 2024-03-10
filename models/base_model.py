1b65989505c8371ec23d428a5dc3db42b4294eb7
hibaasim <hiba.asim10@gamil.com>
2024-03-10 11:13:12 +0200

    task-4

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,25 @@ import models
 class BaseModel():
     '''Defines the basemodel'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Defines the instances
 
         Args:
-            self
+            *args: arguments
+            **kwargs: keyword arguments
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''string that defines the object
