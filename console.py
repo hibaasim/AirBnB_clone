e167f36dbf28b58d539d319f29bf8acb1d1ebdf0
hibaasim <hiba.asim10@gamil.com>
2024-03-10 18:10:22 +0200

    task-6

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Module for command line interpreter'''
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    '''Defines the console'''
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        '''Exits the program'''
+        return True
+
+    def do_EOF(self, args):
+        '''Exits the program'''
+        return True
+
+    def do_emptyline(self):
+        '''Passes an empty line and doesn't do anything'''
+        pass
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
