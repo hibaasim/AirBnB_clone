9c5c7c65c204f0f0a333f42c392e16ac2cd4b106
hibaasim <hiba.asim10@gamil.com>
2024-03-11 05:00:55 +0200

    8

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''User module'''
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    '''Inherits from BaseModel
+
+    Attributes:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+    '''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
