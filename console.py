71e5b536f611c0ebccdf70a538bee55819c03be9
hibaasim <hiba.asim10@gmail.com>
2024-05-20 02:44:35 +0300

    task-7

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 '''The console module'''
 import cmd
-
+import shlex
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     '''Defines the console
@@ -22,6 +24,126 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         '''Does not exectue anything when enter is pressed in an empty line'''
         pass
+
+    def do_create(self, args):
+        '''Creates a new instance'''
+        argument = shlex.split(args)
+
+        if len(argument) == 0:
+            print('** class name missing **')
+        elif argument[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            new_inst = eval(f'{argument[0]}()')
+            storage.save()
+            print(new_inst.id)
+
+    def do_show(self, args):
+        '''Prints the string representation of an instance'''
+        argument = shlex.split(args)
+
+        if len(argument) == 0:
+            print('** class name missing **')
+        elif argument[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        elif len(argument) < 2:
+            print('** instance id missing **')
+        else:
+            instance = storage.all()
+
+        key = f'{argument[0]}.{argument[1]}'
+        if key in instance:
+            print(instance[key])
+        else:
+            print('** no instance found **')
+
+    def do_destroy(self args):
+        ''' Deletes an instance based on the class name and id'''
+        argument = shlex.split(args)
+
+        if len(argument) == 0:
+            print('** class name missing **')
+        elif argument[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        elif len(argument) < 2:
+            print('** instance id missing **')
+        else:
+            instance = storage.all()
+
+        key = f'{argument[0]}.{argument[1]}'
+        if key in instance:
+            del instance[key]
+            storage.save()
+        else:
+            print('** no instance found **')
+
+
+     def do_all(self, args):
+        '''Prints all string representation of all instances'''
+        instance = storage.all()
+        argument = shlex.split(args)
+
+        if len(argument) == 0:
+            for key, value in instance.items():
+                print(str(value))
+        elif argument[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            for key, value in instance.items():
+                if key.split('.')[0] == argument[0]:
+                    print(str(value))
+
+    def do_update(self, args) def do_all(self, args):
+        '''Prints all string representation of all instances'''
+        instance = storage.all()
+        argument = shlex.split(args)
+
+        if len(argument) == 0:
+            for key, value in instance.items():
+                print(str(value))
+        elif argument[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            for key, value in instance.items():
+                if key.split('.')[0] == argument[0]:
+                    print(str(value))
+
+    def do_update(self, args):
+        '''Updates an instance based on the class name and id
+
+        Usage:
+            update <class name> <id> <attribute name> "<attribute value>"
+        '''
+        argument = shlex.split(args)
+
+        if len(argument) == 0:
+            print('** class name missing **')
+        elif argument[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        elif len(argument) < 2:
+            print('** instance id missing **')
+        else:
+            instance = storage.all()
+
+            key = f'{argument[0]}.{argument[1]}'
+            if key not in instance:
+                print('** no instance found **')
+            elif len(argument) < 3:
+                print('** attribute name missing **')
+            elif len(argument) < 4:
+                print('** value missing **')
+            else:
+                obj = instance[key]
+                attr_name = argument[2]
+                attr_val = argument[3]
+
+                try:
+                    attr_val = eval(attr_val)
+                except Exception:
+                    pass
+                setattr(obj, attr_name, attr_val)
+                obj.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
